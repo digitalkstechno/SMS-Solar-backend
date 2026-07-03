2b5b51b3889ec6a87f072fe67d9b6ca88c933437
manavdigitalks <manavdigitalks@gmail.com>
2026-07-03 11:34:56 +0530

    wp integration done pdf issue fix

--- routes/indexv1.js
+++ routes/indexv1.js
@@ -21,5 +21,6 @@ router.use("/stock", require("./stockTransaction"));
 router.use("/users", require("./user"));
 router.use("/project-detail", require("./projectDetail"));
 router.use("/analytics", require("./analytics"));
+router.use("/quotation", require("./quotation"));
 
 module.exports = router;
